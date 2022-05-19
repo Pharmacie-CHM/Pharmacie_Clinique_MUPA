1aecfde9ba95eb56bd9e40ad4b83f89fb21a7827
Pharmacie-CHM <105777370+Pharmacie-CHM@users.noreply.github.com>
2022-05-19 09:35:42 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,23 +1,33 @@
 import streamlit as st
 import pandas as pd
 
-logo = st.sidebar.image('logo_chm.png')
+logo = st.sidebar.image('img/logo_chm.png')
 st.sidebar.caption("Guide d'analyse pharmacothérapeutique chez le patient MUPA.")
-st.subheader('À adapter selon le contexte')
 
 data_frame = pd.read_csv('Analyse Pharmacotherapeutique File.csv')
 data_frame.set_index('Index', inplace=True)
 
+liste_medoc = [str(medoc) for medoc in set(data_frame.index)]
+liste_medoc.sort()
+     
+
 option = st.sidebar.selectbox(
-     'Choisis un médicament',
-     set(data_frame.index))
+     "Choisis un médicament. Petite astuce : Il suffit de cliquer sur la barre de recherche (pas besoin d'effacer) et de taper les première lettres du"
+     " médicament (DCI ou Princeps).",
+     liste_medoc)
 
-st.sidebar.write('Tu as sélectionné:', option)
+col1, col2 = st.columns(2)
 
-compteur = 0
-for i in data_frame.index: 
-    if i == option:
-        compteur += 1
+if st.sidebar.button("Ajouter le médicament"):
+     col1.subheader('Liste des prescriptions')
+     col1.write(option)
+     with col2 :
+          st.subheader('À adapter selon le contexte')
+          with st.expander("Commentaires"):                                  
+               compteur = 0
+               for i in data_frame.index: 
+                   if i == option:
+                       compteur += 1
 
-for i in range(compteur):
-    txt = st.text_area(f"{data_frame.loc[{option}, 'Condition'][i]}", f"{data_frame.loc[{option}, 'Paragraphe'][i]}", max_chars=500)
+               for i in range(compteur):
+                   txt = st.text_area(f"{data_frame.loc[{option}, 'Condition'][i]}", f"{data_frame.loc[{option}, 'Paragraphe'][i]}", max_chars=500)
